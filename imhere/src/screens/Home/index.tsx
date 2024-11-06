e06b63e5a7fbcfd58c621a1c840f78e4ec3623b5
Jhonanthan <jhonanthanwork@gmail.com>
2024-11-05 21:14:29 -0300

    Primeiro Commit

---
+++ imhere/src/screens/Home/index.tsx
@@ -0,0 +1,27 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+
+export function Home() {
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão adicionar");
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do Participante"
+        placeholderTextColor="#6b6b6b"
+        // keyboardType="numeric" // altera o tipo de teclado, apenas números, apenas texto e outros tipos
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}> + </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
