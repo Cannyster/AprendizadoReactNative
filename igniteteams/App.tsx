fa1c27144a7231807b73e387b2bcba25f086d7e4
Jhonanthan <jhonanthanwork@gmail.com>
2024-11-21 13:28:35 -0300

    Criando tema para aplicação

--- igniteteams/App.tsx
+++ igniteteams/App.tsx
@@ -1,5 +1,11 @@
+import { ThemeProvider } from "styled-components";
 import { Groups } from "@screens/Groups/index";
+import themeDefault from "./src/theme/index";
 
 export default function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={themeDefault}>
+      <Groups />
+    </ThemeProvider>
+  );
 }
