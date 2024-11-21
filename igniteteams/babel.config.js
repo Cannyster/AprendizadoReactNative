7ef33971ca2033697dd5b2e222c0f43a9bccf855
Jhonanthan <jhonanthanwork@gmail.com>
2024-11-21 11:12:19 -0300

    adicionando bibliotecas para resolver problemas, config adicionais para o babel(transpilador)

--- igniteteams/babel.config.js
+++ igniteteams/babel.config.js
@@ -1,8 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: [
+      ["babel-preset-expo"],
+      ["module:metro-react-native-babel-preset"],
+    ],
     plugins: [
+      ["babel-plugin-styled-components"],
       [
         "module-resolver",
         {
