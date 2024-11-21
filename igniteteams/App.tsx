40ab81e31c0b10845b67961b9a2fc19b4e4ba5ea
Jhonanthan <jhonanthanwork@gmail.com>
2024-11-21 16:01:22 -0300

    Ajustes para o tema funcionar com autocomplete do typescript

--- igniteteams/App.tsx
+++ igniteteams/App.tsx
@@ -1,10 +1,10 @@
 import { ThemeProvider } from "styled-components";
 import { Groups } from "@screens/Groups/index";
-import themeDefault from "./src/theme/index";
+import theme from "./src/theme/index";
 
 export default function App() {
   return (
-    <ThemeProvider theme={themeDefault}>
+    <ThemeProvider theme={theme}>
       <Groups />
     </ThemeProvider>
   );
